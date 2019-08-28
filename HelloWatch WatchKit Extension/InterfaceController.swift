55542fbe05db5582c7601c0b85c988505a71995d
ikeda-mk <2306915+ikeda-mk@users.noreply.github.com>
2019-08-29 01:53:33 +0900

    apple watch アプリでGPS取得

--- HelloWatch WatchKit Extension/InterfaceController.swift
+++ HelloWatch WatchKit Extension/InterfaceController.swift
@@ -9,23 +9,44 @@
 import WatchKit
 import Foundation
 
-
 class InterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var latLabel: WKInterfaceLabel!
+    @IBOutlet weak var lonLabel: WKInterfaceLabel!
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+
         // Configure interface objects here.
+
+
+        LocationManager.Singleton.sharedInstance.startUpdatingLocation()
+
+        let center = NotificationCenter.default
+        center.addObserver(self,
+                selector: #selector(type(of: self).fetchStation(notification:)),
+                name: NSNotification.Name(rawValue: LMLocationUpdateNotification),
+                object: nil)
     }
-    
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+
     }
-    
+
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
 
+    @objc func fetchStation(notification: Notification) {
+        let infoDic: Dictionary = notification.userInfo!
+        let location: CLLocation? = infoDic[LMLocationInfoKey] as? CLLocation
+        let coordinate = location!.coordinate
+
+        self.latLabel.setText(coordinate.latitude.description)
+        self.lonLabel.setText(coordinate.longitude.description)
+
+    }
 }
--- HelloWatch/ViewController.swift
+++ HelloWatch/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        // Do any additional setup after loading the view, typically from a nib.
+        LocationManager.Singleton.sharedInstance.startUpdatingLocation()
+
+        let center = NotificationCenter.default
+        center.addObserver(self,
+                selector: #selector(type(of: self).fetch(notification:)),
+                name: NSNotification.Name(rawValue: LMLocationUpdateNotification),
+                object: nil)        // Do any additional setup after loading the view, typically from a nib.
+        LocationManager.Singleton.sharedInstance.startUpdatingLocation()
+
+    }
+
+    @objc func fetch(notification: Notification) {
+        let infoDic: Dictionary = notification.userInfo!
+        let location: CLLocation? = infoDic[LMLocationInfoKey] as? CLLocation
+        let coordinate = location!.coordinate
+
+        let a = (coordinate.latitude).description
+        let b = (coordinate.longitude).description
+
+        self.label1.text = "aaa"
+        self.label1.text = (coordinate.latitude).description
+        self.label2.text = (coordinate.longitude).description
     }
 
 
