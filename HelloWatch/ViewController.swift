08278c0d239ccbd849439f0a5d5beebb8d2600a3
ikeda-mk <2306915+ikeda-mk@users.noreply.github.com>
2019-08-28 23:41:59 +0900

    initial commit

---
+++ HelloWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWatch
+//
+//  Created by mak on 2019/08/28.
+//  Copyright © 2019 ikedama.dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
